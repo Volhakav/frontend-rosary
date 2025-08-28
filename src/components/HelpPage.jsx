652693a69638493a9f09b11a1da6ec3267617ea7
Volhakov <olkakovka812@gmail.com>
2025-08-28 14:10:58 +0200

    add responese for help section

--- src/components/HelpPage.jsx
+++ src/components/HelpPage.jsx
@@ -23,6 +23,84 @@ const HelpPage = () => {
     }
   };
 
+  const renderItem = (item) => {
+    switch (item.type) {
+      case "Text":
+        return (
+          <div key={item.id} className="help-item help-text">
+            <div
+              className="help-text-content"
+              dangerouslySetInnerHTML={{ __html: item.value }}
+            />
+          </div>
+        );
+
+      case "Image":
+        return (
+          <div key={item.id} className="help-item help-image">
+            <div className="help-image-container">
+              <img
+                src={item.value}
+                alt={item.options?.alt || "Ilustracja pomocy"}
+                className="help-image-element"
+              />
+            </div>
+          </div>
+        );
+
+      case "Video": {
+        let cleanUrl = item.value;
+        if (cleanUrl.startsWith("hhttps://")) {
+          cleanUrl = cleanUrl.replace("hhttps://", "https://");
+        }
+
+        let embedUrl = cleanUrl;
+        if (cleanUrl.includes("youtube.com/watch?v=")) {
+          const videoId = cleanUrl.split("v=")[1].split("&")[0];
+          embedUrl = `https://www.youtube.com/embed/${videoId}`;
+        } else if (cleanUrl.includes("youtu.be/")) {
+          const videoId = cleanUrl.split("youtu.be/")[1].split("?")[0];
+          embedUrl = `https://www.youtube.com/embed/${videoId}`;
+        }
+
+        return (
+          <div key={item.id} className="help-item help-video">
+            <div className="video-container">
+              <iframe
+                width="100%"
+                height="360"
+                src={embedUrl}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+          </div>
+        );
+      }
+
+      case "Game":
+        return (
+          <div key={item.id} className="help-item help-game">
+            <div className="game-container">
+              <iframe
+                src={item.value}
+                title="Gra"
+                width="100%"
+                height="360"
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+            </div>
+          </div>
+        );
+
+      default:
+        return null;
+    }
+  };
+
   if (loading) {
     return <div className="help-loading">Ładowanie pomocy...</div>;
   }
@@ -35,31 +113,7 @@ const HelpPage = () => {
     <div className="help-page">
       <h1>Pomoce</h1>
       <div className="help-content-container">
-        {helpData.data?.map((item) => {
-          if (item.type === "Text") {
-            return (
-              <div key={item.id} className="help-item help-text">
-                <div 
-                  className="help-text-content"
-                  dangerouslySetInnerHTML={{ __html: item.value }} 
-                />
-              </div>
-            );
-          } else if (item.type === "Image") {
-            return (
-              <div key={item.id} className="help-item help-image">
-                <div className="help-image-container">
-                  <img 
-                    src={item.value} 
-                    alt={item.options?.alt || "Ilustracja pomocy"} 
-                    className="help-image-element"
-                  />
-                </div>
-              </div>
-            );
-          }
-          return null;
-        })}
+        {helpData.data?.map((item) => renderItem(item))}
       </div>
     </div>
   );
