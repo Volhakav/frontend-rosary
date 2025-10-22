af1f7b9d47e0f20284d7910e2e59e2ce92cf3c14
Volhakov <olkakovka812@gmail.com>
2025-10-22 21:32:41 +0200

    changed HalpPage into static page

--- src/components/HelpPage.jsx
+++ src/components/HelpPage.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
-
+import Piosenka from "../images/Piosenka.png"
+import Modlitwa from "../images/Modlitwa_R.png"
+import Pomoc from "../images/Pomoc.png"
+/*
 const HelpPage = () => {
   const [helpData, setHelpData] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -118,5 +121,193 @@ const HelpPage = () => {
     </div>
   );
 };
+*/
 
-export default HelpPage;
+export default function HelpPage(){
+  return(
+    <div className="song-description-container">
+      <section className="song-container">
+        <h2 className="song-title">PIOSENKA</h2>
+
+        <p className="song-intro">
+          Przygotowany został specjalny śpiew dla dzieci. Nosi on tytuł
+          „Święty różaniec”. Refren podkreśla jego świętość, a
+          także wyraża prawdę, że przeżywamy go z Maryją i że prowadzi nas on do
+          Boga. Pierwsza zwrotka przypomina, że różaniec jest darem oraz wskazuje
+          na poszczególne jego części. W drugiej zwrotce dzieci wyrażają swoje
+          zaangażowanie w tę modlitwę.
+        </p>
+
+        <div className="song-lyrics">
+          <p className="refrain">
+            Ref: Różaniec, święty różaniec! Różaniec z Maryją, różaniec! <br />
+            Różaniec, święty różaniec! Do Boga prowadzi różaniec!
+          </p>
+
+          <div className="verse">
+            <p>
+              1.Wielki dar Boga, cudowna droga, <br />
+              Matka prowadzi, dzieci gromadzi. <br />
+              Radość nam głosi, światło przynosi, <br />
+              w krzyżu pomaga, niebo wybłaga.
+            </p>
+          </div>
+
+          <div className="verse">
+            <p>
+              2.Zło pokonuję, drogi prostuję, <br />
+              siebie nawracam, pokój przywracam. <br />
+              „Zdrowaś” powtarzam, sercem rozważam, <br />
+              łaski wypraszam, chwałę ogłaszam.
+            </p>
+          </div>
+        </div>
+        <div className="song-image">
+          <img src={Piosenka} alt="Piosenka"></img>
+        </div>
+        <p>Poniżej ten śpiew w wykonaniu zespołu ZIARENKO z parafii Podstolice.</p>
+        <a href="https://youtu.be/DVb97zGXmLs">https://youtu.be/DVb97zGXmLs</a>
+      </section>
+      <section className="description-container">
+        <h2 className="description-title">OPIS</h2>
+
+        <p>
+          Jak pomóc dzieciom wejść w modlitwę różańcową? Jak zaprosić je do
+          codziennego odmawiania dziesiątka różańca? Piękną inicjatywę{" "}
+          <strong>Podwórkowych Kółek Różańcowych</strong> –{" "}
+          <a href="https://www.pkrd.pl/" target="_blank" rel="noopener noreferrer">
+            www.pkrd.pl
+          </a>{" "}
+          – podjęła przed wieloma laty i kontynuuje Madzia Buczek. W parafiach
+          podejmowane są różne działania w tym względzie.
+        </p>
+
+        <p>
+          We wspólnocie Żywego Różańca Archidiecezji Krakowskiej powstała kolejna
+          propozycja. Teksty drukowane są dostępne w wydawnictwie św. Stanisława
+          BM –{" "}
+          <a
+            href="https://stanislawbm.pl/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            stanislawbm.pl
+          </a>
+          .
+        </p>
+
+        <h3>Pomoce dla prowadzących</h3>
+        <p>
+          Dla prowadzących rozmowy z dziećmi w domu i na spotkaniach w grupie
+          przygotowany został podręcznik{" "}
+          <strong>„Wprowadzenie dzieci w modlitwę różańcową”</strong> (więcej) –{" "}
+          <a
+            href="https://zr.diecezja.pl/roze-dzieci/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            zr.diecezja.pl/roze-dzieci
+          </a>
+          .
+        </p>
+        <img src={Modlitwa} alt="Modlitwa"></img>
+
+        <h3>Pomoce dla dzieci – drukowane</h3>
+        <p>
+          Dla dzieci przygotowane są pomoce dwojakiego rodzaju: drukowane i
+          elektroniczne. Te pierwsze są następujące:
+        </p>
+        <ul>
+          <li>
+            Kartki zawierające myśli do przeżywania każdej z 20 tajemnic różańca;
+          </li>
+          <li>
+            Skoroszyt, do którego dzieci wkładają kolejne kartki z opisem
+            różańcowych tajemnic;
+          </li>
+          <li>
+            Obrazki z tytułem, jaki dziecku można nadać, gdy ukończy przeżywanie
+            części różańca (więcej) –{" "}
+            <a
+              href="https://zr.diecezja.pl/roze-dzieci/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              zr.diecezja.pl/roze-dzieci
+            </a>
+            .
+          </li>
+        </ul>
+        <img src={Pomoc} alt="Pomoc"></img>
+
+        <h3>Pomoce dla dzieci – internetowe</h3>
+        <p>
+          Niniejsza strona zawiera pomoce umieszczone w Internecie. Odwiedzający
+          witrynę mogą tam znaleźć: krótkie słowo kapłana i jego błogosławieństwo,
+          modlące się dzieci, śpiewy, proste zadania związane z tematem
+          rozważań, przykłady świętych i inne teksty.
+        </p>
+
+        <p>
+          Wszystkie pomoce służą temu, aby mobilizować dziecko do codziennej
+          modlitwy oraz inspirować je do jej głębszego przeżywania. Dzieci mogą z
+          nich korzystać, wchodząc – za zgodą rodziców – na stronę internetową. Jest
+          ona dostępna w wersji desktopowej oraz mobilnej.
+        </p>
+
+        <p>
+          Na każdej kartce, którą otrzymują dzieci, został zamieszczony adres
+          internetowy i kod QR, pod którym znajdują się pomoce do tej tajemnicy,
+          którą dziecko rozważa, np.{" "}
+          <a
+            href="https://www.zrdzieci.diecezja.pl/radosna/1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            www.zrdzieci.diecezja.pl/radosna/1
+          </a>
+          .
+        </p>
+
+        <p>
+          Po wpisaniu tego adresu lub po zeskanowaniu kodu QR otwiera się od razu
+          okienko z tajemnicą zwiastowania Najświętszej Maryi Pannie, a pod
+          adresem{" "}
+          <a
+            href="https://www.zrdzieci.diecezja.pl/światła/2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            www.zrdzieci.diecezja.pl/światła/2
+          </a>{" "}
+          znajdują się pomoce do rozważania tajemnicy przemiany wody w wino w Kanie
+          Galilejskiej.
+        </p>
+
+        <p>
+          Po wejściu na stronę ukazuje się ten dzień rozważania tajemnicy, który
+          jest aktualnym dniem miesiąca. Zaleca się więc, aby rozpoczynanie
+          rozważania kolejnej tajemnicy różańca następowało zawsze w pierwszym dniu
+          miesiąca. Takie ustawienie strony jest pomocne dla dziecka, które
+          codziennie lub często zagląda na przygotowane pomoce.
+        </p>
+
+        <p>
+          Zawsze też rodzice i osoby pomagające w formacji mogą wejść przez ogólny
+          wykaz części różańca do dowolnej tajemnicy i przejrzeć całość lub wybrać
+          konkretny dzień.
+        </p>
+
+        <p>
+          Rodzice dzieci i osoby pomagające w formacji powinny zapoznać się z
+          propozycjami zamieszczonymi w Internecie i zdecydować, w jaki sposób z
+          nich korzystać.
+        </p>
+      </section>
+    </div>
+    
+  );
+}
+
+
+
